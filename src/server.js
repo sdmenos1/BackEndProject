462492474031c33793135255e7acdaa5bd76e597
sdmenos1 <adr14nm4_30_12@hotmail.com>
2025-07-08 23:07:29 -0500

    refact: corse

--- src/server.js
+++ src/server.js
@@ -48,7 +48,12 @@ app.use(
 
 
 app.use(morgan("combined"))
-
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+)
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
 
